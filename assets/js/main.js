e844c79a8e9fae81b825bf96c23f63137aa908f1
Aadhil8 <98220521+Aadhil8@users.noreply.github.com>
2022-02-01 00:57:41 +0530

    0k

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
-
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -14,9 +10,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
 
@@ -29,9 +22,7 @@
     }
   }
 
-  /**
-   * Scrolls to an element with header offset
-   */
+
   const scrollto = (el) => {
     window.scrollTo({
       top: 0,
@@ -39,18 +30,13 @@
     })
   }
 
-  /**
-   * Mobile nav toggle
-   */
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+
   on('click', '#navbar .nav-link', function(e) {
     let section = select(this.hash)
     if (section) {
@@ -102,9 +88,6 @@
     }
   }, true)
 
-  /**
-   * Activate/show sections on load with hash links
-   */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       let initial_nav = select(window.location.hash)
@@ -132,9 +115,7 @@
     }
   });
 
-  /**
-   * Skills animation
-   */
+
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -149,9 +130,7 @@
     })
   }
 
-  /**
-   * Testimonials slider
-   */
+
   new Swiper('.testimonials-slider', {
     speed: 600,
     loop: true,
@@ -178,9 +157,7 @@
     }
   });
 
-  /**
-   * Porfolio isotope and filter
-   */
+ 
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -206,25 +183,19 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
+
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Initiate portfolio details lightbox 
-   */
+ 
   const portfolioDetailsLightbox = GLightbox({
     selector: '.portfolio-details-lightbox',
     width: '90%',
     height: '90vh'
   });
 
-  /**
-   * Portfolio details slider
-   */
+
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
